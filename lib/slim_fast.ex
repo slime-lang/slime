426b7be3cc7cabf42adb4af2942b3805d16c9705
Sean Callan <seancallan@gmail.com>
2015-07-31 18:16:38 -0700

    Incorporate Parser and Tree

--- lib/slim_fast.ex
+++ lib/slim_fast.ex
@@ -1,2 +1,15 @@
 defmodule SlimFast do
+  import SlimFast.Parser
+  import SlimFast.Tree
+
+  def evaluate(input) do
+    input
+    |> tokenize
+    |> parse_lines
+    |> build_tree
+  end
+
+  defp tokenize(input, delim \\ "\n") do
+    String.split(input, delim)
+  end
 end
--- test/slim_fast_test.exs
+++ test/slim_fast_test.exs
@@ -1,7 +1,10 @@
 defmodule SlimFastTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias SlimFast.Tree.Branch
+
+  test "parse simple nesting" do
+    parsed = "#id.class\n\tp\n\t| Hello World" |> SlimFast.evaluate
+    assert parsed == [%Branch{type: :div, children: [%Branch{type: :p, children: [%Branch{type: :text, children: [], content: "Hello World"}]}], id: "id", css: ["class"]}]
   end
 end
