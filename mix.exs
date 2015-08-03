b13f504af33c11081c639c64bf11dbe0f34013a4
Sean Callan <seancallan@gmail.com>
2015-08-02 20:15:10 -0700

    Prep 0.1 for hex

--- mix.exs
+++ mix.exs
@@ -3,30 +3,19 @@ defmodule SlimFast.Mixfile do
 
   def project do
     [app: :slim_fast,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
-  end
-
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
-  def application do
-    [applications: [:logger]]
-  end
-
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
-  defp deps do
-    []
+     deps: [],
+     description: """
+     An Elixir library for rendering slim templates.
+     """,
+     package: [
+       files: ["lib", "mix.exs", "README*", "LICENSE*"],
+       contributors: ["Sean Callan"],
+       licenses: ["MIT"],
+       links: %{ "Github" => "https://github.com/doomspork/slim_fast" }
+     ]]
   end
 end
