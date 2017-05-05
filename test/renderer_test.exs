cd9f9286de2cb912536509173ac0492bb0741b58
Alexander Stanko <astanko@aviasales.ru>
2017-05-05 11:48:27 +0700

    Handle nested inline tags in peg parser

--- test/renderer_test.exs
+++ test/renderer_test.exs
@@ -83,60 +83,6 @@ defmodule RendererTest do
     assert render(@inline_slime, articles: articles) == @inline_html
   end
 
-  test "render inline tags" do
-    slime = ~s"""
-    ul
-      li#ll.first: a href="/a" A link
-      li: a href="/b" B link
-    """
-
-    assert render(slime) == """
-    <ul>
-    <li id="ll" class="first"><a href="/a">A link</a></li>
-    <li><a href="/b">B link</a></li>
-    </ul>
-    """ |> String.replace("\n", "")
-  end
-
-  test "render nested inline html" do
-    slime = ~s"""
-    .row: .col-lg-12: p Hello World
-    """
-
-    assert render(slime) == """
-    <div class="row">
-    <div class="col-lg-12">
-    <p>Hello World</p>
-    </div>
-    </div>
-    """ |> String.replace("\n", "")
-  end
-
-  test "render mixed nesting" do
-    slime = ~s"""
-    .wrap: .row: .col-lg-12
-      .box: p One
-      .box: p Two
-    p Three
-    """
-
-    assert render(slime) == """
-    <div class="wrap">
-    <div class="row">
-    <div class="col-lg-12">
-    <div class="box">
-    <p>One</p>
-    </div>
-    <div class="box">
-    <p>Two</p>
-    </div>
-    </div>
-    </div>
-    </div>
-    <p>Three</p>
-    """ |> String.replace("\n", "")
-  end
-
   test "render closed tag (ending with /)" do
     assert render(~s(img src="image.png"/)) == ~s(<img src="image.png"/>)
   end
