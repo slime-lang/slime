698e29ab85aaabc098fc76717131d438cf42ac23
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-05-20 22:07:25 +0700

    multiline comments

--- test/rendering/comments_test.exs
+++ test/rendering/comments_test.exs
@@ -1,18 +1,48 @@
 defmodule RenderCommentsTest do
   use ExUnit.Case, async: true
 
-  import Slime, only: [render: 1]
+  import Slime, only: [render: 1, render: 2]
 
-  test "lines started with / are commented out" do
+  test "lines indented deeper than / are commented out" do
     slime = """
-    / code comment
+    / Code comment
+      that spans over multiple lines
+    /
+     Code comment started
+     on another line
     p.test
+    / One-liner
     """
     assert render(slime) == ~s(<p class="test"></p>)
   end
 
   test "/! renders html comments" do
     assert render(~s(/! html comment)) == ~s(<!--html comment-->)
+  end
+
+  test "/! can span over multiple lines" do
+    slime = """
+    div
+      /!
+         HTML comments
+      /! Have similar semantics
+          to other text blocks:
+              they can be nested, with indentation being converted to spaces
+    """
+    html = """
+    <div><!--HTML comments--><!--Have similar semantics
+     to other text blocks:
+         they can be nested, with indentation being converted to spaces--></div>
+    """ |> String.strip(?\n)
+    assert render(slime) == html
+  end
+
+  test "/! renders comments with interpolation" do
+    slime = ~S(/! html comment with #{interpolation})
+    html = """
+    <!--html comment with a-->
+    """ |> String.strip(?\n)
+    assert render(slime, interpolation: "a") == html
   end
 
   test "/![foo] renders IE comments" do
